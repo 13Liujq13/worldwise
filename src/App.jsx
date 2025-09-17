ea7bf0db115957d924b1d8fdc57d4ae20690a5cd
Eason <ljq13308001658@163.com>
2025-09-17 17:13:25 +0800

    feat：懒加载/按需加载

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Product from './pages/Product';
-import HomePage from './pages/HomePage';
-import PageNotFound from './pages/PageNotFound';
-import Pricing from './pages/Pricing';
-import AppLayout from './pages/appLayout';
-import Login from './pages/Login';
+import { BrowserRouter, Routes, Route, lazy, Suspense } from 'react-router-dom';
+// import Product from './pages/Product';
+// import HomePage from './pages/HomePage';
+// import PageNotFound from './pages/PageNotFound';
+// import Pricing from './pages/Pricing';
+// import AppLayout from './pages/appLayout';
+// import Login from './pages/Login';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
@@ -12,33 +12,42 @@ import Form from './components/Form';
 import { CitiesProvider } from './context/CitiesContext';
 import { AuthProvider } from './context/FeakAuthContext';
 import ProtectRoute from './pages/ProtectRoute';
+import Spinner from './components/Spinner';
+const HomePage = lazy(() => import('./pages/HomePage'));
+const Product = lazy(() => import('./pages/Product'));
+const Pricing = lazy(() => import('./pages/Pricing'));
+const AppLayout = lazy(() => import('./pages/appLayout'));
+const Login = lazy(() => import('./pages/Login'));
+const PageNotFound = lazy(() => import('./pages/PageNotFound'));
 
 function App() {
   return (
     <CitiesProvider>
       <AuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<HomePage />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route
-              path="app"
-              element={
-                <ProtectRoute>
-                  <AppLayout />
-                </ProtectRoute>
-              }
-            >
-              <Route index element={<CityList />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route index element={<HomePage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectRoute>
+                    <AppLayout />
+                  </ProtectRoute>
+                }
+              >
+                <Route index element={<CityList />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="login" element={<Login />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </AuthProvider>
     </CitiesProvider>
